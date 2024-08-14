762f68d55d52fefdf60268e81af158c27ddabeff
steveLin43 <tai45612360@gmail.com>
2024-08-14 15:34:11 +0800

    RESTful api init

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 使用 gin 架構進行開發
 
 
-建立相關套件管理 & 初始化模塊
+### 建立相關套件管理 & 初始化模塊
 
 > go mod init 專案名稱
 > go get -u github.com/gin-gonic/gin@v1.6.3
 
-2-10
+### 驗證服務是否正常
+> go run main.go
---
+++ main.go
@@ -0,0 +1,11 @@
+func main() {
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:			":8080",
+		Handler:		router,
+		ReadTimeout:	10 * time.Second,
+		WriteTimeout:	10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
+}
